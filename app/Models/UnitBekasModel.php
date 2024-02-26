3b9133edf097189e1c8e7ecd881127fcd41368dd
dapurcode403 <dapurcode403@gmail.com>
2024-02-26 08:33:00 +0700

    d

--- app/Models/UnitBekasModel.php
+++ app/Models/UnitBekasModel.php
@@ -9,7 +9,7 @@ class UnitBekasModel extends Model
     protected $table            = 'tbl_unit_bekas';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields    = ['id', 'foto', 'nama_kontak', 'no_wa', 'alamat', 'longitude', 'latitude', 'ins_by', 'status', 'created_at', 'updated_at'];
+    protected $allowedFields    = ['id', 'foto', 'nama_kontak', 'no_wa', 'alamat', 'catatan', 'longitude', 'latitude', 'ins_by', 'status', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = true;
