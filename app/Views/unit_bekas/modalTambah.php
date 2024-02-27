dbe2787a8fa9a70e691703da4166ef176cf68762
dapurcode403 <dapurcode403@gmail.com>
2024-02-27 07:46:19 +0700

    s

--- app/Views/unit_bekas/modalTambah.php
+++ app/Views/unit_bekas/modalTambah.php
@@ -18,10 +18,8 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                             </div>
-                            <input type="text" class="form-control float-right tanggal" id="tglAktivitas2"
-                                name="tglAktivitas2" disabled>
-                            <input type="hidden" class="form-control float-right tanggal" id="tglAktivitas"
-                                name="tglAktivitas">
+                            <input type="text" class="form-control float-right tanggal" id="tglAktivitas2" name="tglAktivitas2" disabled>
+                            <input type="hidden" class="form-control float-right tanggal" id="tglAktivitas" name="tglAktivitas">
                             <div id="errortglFollowup" class="invalid-feedback">
                             </div>
                         </div>
@@ -32,35 +30,21 @@
                     <label for="aktivitas" class="col-sm-3 col-form-label">Picture</label>
                     <div class="col-sm-8">
                         <style>
-                        .my_camera,
-                        .my_camera video {
-                            display: inline-block;
-                            width: 100% !important;
-                            margin: auto;
-                            height: auto !important;
-                            border-radius: 15px;
-                        }
+                            .my_camera,
+                            .my_camera video {
+                                display: inline-block;
+                                width: 100% !important;
+                                margin: auto;
+                                height: auto !important;
+                                border-radius: 15px;
+                            }
                         </style>
                         <div class="my_camera"></div>
-                        <button type="button" class="btn btn-success btn-block btn-flat" onclick="take_picture();"><i
-                                class="fas fa-camera-retro">
+                        <button type="button" class="btn btn-success btn-block btn-flat" onclick="take_picture();"><i class="fas fa-camera-retro">
                                 Take
                                 Foto</i></button>
                     </div>
                 </div>
-
-                <div class="form-group row">
-                    <label for="aktivitas" class="col-sm-3 col-form-label">Aktivitas</label>
-                    <div class="col-sm-8">
-                        <textarea class="form-control" id="keterangan" name="keterangan" rows="4" required
-                            oninvalid="this.setCustomValidity('Keterangan tidak boleh kosong');"
-                            onchange="try{setCustomValidity('')}catch(e){};"
-                            x-moz-errormessage="Keterangan tidak boleh kosong"></textarea>
-                        <div id="errorketerangan" class="invalid-feedback">
-                        </div>
-                    </div>
-                </div>
-
 
                 <div class="form-group row">
                     <label for="aktivitas" class="col-sm-3 col-form-label"></label>
@@ -69,6 +53,18 @@
                     </div>
                     <input type="hidden" name="imagecam" id="imagecam" class="image-tag">
                 </div>
+
+                <div class="form-group row">
+                    <label for="aktivitas" class="col-sm-3 col-form-label">Aktivitas</label>
+                    <div class="col-sm-8">
+                        <textarea class="form-control" id="keterangan" name="keterangan" rows="4" required oninvalid="this.setCustomValidity('Keterangan tidak boleh kosong');" onchange="try{setCustomValidity('')}catch(e){};" x-moz-errormessage="Keterangan tidak boleh kosong"></textarea>
+                        <div id="errorketerangan" class="invalid-feedback">
+                        </div>
+                    </div>
+                </div>
+
+                <div id="map_location"></div>
+
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-primary btnSimpan">Simpan</button>
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
@@ -82,89 +78,110 @@
     </div>
 </div>
 <script>
-Webcam.set({
-    width: 240,
-    height: 320,
-    dest_width: 240,
-    dest_height: 320,
-    image_format: 'jpeg',
-    jpeg_quality: 90,
-});
-Webcam.attach('.my_camera');
-
-function take_picture() {
-    Webcam.snap(function(data_uri) {
-        $(".image-tag").val(data_uri);
-        document.getElementById('result').innerHTML = '<img src="' + data_uri + '"/>';
-    })
-}
-</script>
-
-<script>
-$(document).ready(function() {
-    $(function() {
-        $('.tanggal').daterangepicker({
-            singleDatePicker: true,
-            autoApply: true,
-            readOnly: true,
-            locale: {
-                format: 'DD-MM-YYYY'
-            }
-        });
+    Webcam.set({
+        width: 240,
+        height: 320,
+        dest_width: 240,
+        dest_height: 320,
+        image_format: 'jpeg',
+        jpeg_quality: 90,
     });
-});
+    Webcam.attach('.my_camera');
+
+    function take_picture() {
+        Webcam.snap(function(data_uri) {
+            $(".image-tag").val(data_uri);
+            document.getElementById('result').innerHTML = '<img src="' + data_uri + '"/>';
+        })
+    }
 </script>
 
 <script>
-$(document).ready(function() {
-    $('.form').submit(function(e) {
-        e.preventDefault();
-
-        let form = $('.form')[0];
-        let data = new FormData(form);
-
-        if ($('#imagecam').val() === '') {
-            Swal.fire({
-                title: "Kamu belum foto aktivitas",
-                text: "Foto dulu ya... 😉",
-            });
-        } else {
-            $.ajax({
-                type: "post",
-                url: $(this).attr('action'),
-                // data: $(this).serialize(),
-                data: data,
-                enctype: 'multipart/form-data',
-                processData: false,
-                contentType: false,
-                cache: false,
-                dataType: "json",
-                beforeSend: function() {
-                    $('.btnSimpan').attr('disabled', 'disabled');
-                    $('.btnSimpan').html('<i class="fa fa-spin fa-spinner"></i> simpan...');
-                },
-                complete: function() {
-                    $('.btnSimpan').removeAttr('disabled');
-                    $('.btnSimpan').html('Simpan');
-                },
-                success: function(response) {
-                    $('#modalTambah').modal('hide');
-                    Swal.fire({
-                        icon: 'success',
-                        title: response.success,
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                    loadDataAktivitas();
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+    $(document).ready(function() {
+        $(function() {
+            $('.tanggal').daterangepicker({
+                singleDatePicker: true,
+                autoApply: true,
+                readOnly: true,
+                locale: {
+                    format: 'DD-MM-YYYY'
                 }
             });
-            return false;
-        }
-
-
+        });
     });
-});
+</script>
+
+<script>
+    $(document).ready(function() {
+        $('.form').submit(function(e) {
+            e.preventDefault();
+
+            let form = $('.form')[0];
+            let data = new FormData(form);
+
+            if ($('#imagecam').val() === '') {
+                Swal.fire({
+                    title: "Kamu belum foto aktivitas",
+                    text: "Foto dulu ya... 😉",
+                });
+            } else {
+                $.ajax({
+                    type: "post",
+                    url: $(this).attr('action'),
+                    // data: $(this).serialize(),
+                    data: data,
+                    enctype: 'multipart/form-data',
+                    processData: false,
+                    contentType: false,
+                    cache: false,
+                    dataType: "json",
+                    beforeSend: function() {
+                        $('.btnSimpan').attr('disabled', 'disabled');
+                        $('.btnSimpan').html('<i class="fa fa-spin fa-spinner"></i> simpan...');
+                    },
+                    complete: function() {
+                        $('.btnSimpan').removeAttr('disabled');
+                        $('.btnSimpan').html('Simpan');
+                    },
+                    success: function(response) {
+                        $('#modalTambah').modal('hide');
+                        Swal.fire({
+                            icon: 'success',
+                            title: response.success,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        loadDataAktivitas();
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                    }
+                });
+                return false;
+            }
+
+
+        });
+    });
+</script>
+
+<script>
+    findMyCoordinates();
+
+    function findMyCoordinates() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    console.log(position.coords.latitude, position.coords.longitude);
+                    $('#map_location').html('<iframe width="100%" height="200" src="https://maps.google.com/maps?q=' +
+                        position.coords.latitude + ',' + position.coords.longitude + '&output=embed"></iframe>');
+                },
+                (error) => {
+                    alert(error.message);
+                }
+            );
+        } else {
+            alert('Geolocation is not available');
+        }
+    }
 </script>
